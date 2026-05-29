e21df0f1dcd1ed62ac4babcc02fe59a214bf4a38
Wn0fbg <temakozlovofficiall@gmail.com>
2026-05-29 22:15:43 +0300

    create any functional for backend

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-import express from "express"; // ✅ Correct
+import express from "express";
 import { config } from "dotenv";
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 
 config({ path: "./config/config.env" });
 
-const app = express();
+// Парсер кук и тела
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Настройка загрузки файлов через multer
+const storage = multer.diskStorage({
+  destination: (req, file, cd) => {
+    cd(null, path.join(__dirname, "uploads", "products_images"));
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const name = Date.now() + "-" + Math.round(Math.random() * 1e9) + ext;
+  },
+});
+
+app.use(errorMiddleware);
 
 export default app;
