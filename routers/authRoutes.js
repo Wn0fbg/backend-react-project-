2af72bb3885457642daccf18f3198f6effd7115a
Wn0fbg <temakozlovofficiall@gmail.com>
2026-05-31 22:19:27 +0300

    debaging

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { register } from "../controllers/authController.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/login", login);
 
 export default router;
